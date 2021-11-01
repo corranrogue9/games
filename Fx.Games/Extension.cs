c58117eb2d7fe13f4ab083632c9c6f32927953da
corranrogue9 <corranrogue9@gmail.com>
2021-10-31 23:23:21 -0700

    added strategy to minimize your opponent's moves; added strategy to maximize your own moves

---
+++ Fx.Games/Extension.cs
@@ -0,0 +1,43 @@
+﻿namespace Fx.Games
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <threadsafety static="true" instance="true"/>
+    internal static class Extension
+    {
+        public static T Minimum<T>(this IEnumerable<T> source, Func<T, double> selector)
+        {
+            using (var enumerator = source.GetEnumerator())
+            {
+                if (!enumerator.MoveNext())
+                {
+                    throw new InvalidOperationException("TODO");
+                }
+
+                var minElement = enumerator.Current;
+                var minValue = selector(minElement);
+                while (enumerator.MoveNext())
+                {
+                    var currentElement = enumerator.Current;
+                    var currentValue = selector(currentElement);
+                    if (currentValue < minValue)
+                    {
+                        minElement = currentElement;
+                        minValue = currentValue;
+                    }
+                }
+
+                return minElement;
+            }
+        }
+
+        public static T Maximum<T>(this IEnumerable<T> source, Func<T, double> selector)
+        {
+            return Minimum(source, (element) => selector(element) * -1);
+        }
+    }
+}
---
+++ Fx.Games/MinimizeMovesStrategy{TGame, TBoard, TMove, TPlayer}.cs
@@ -0,0 +1,22 @@
+﻿namespace Fx.Games
+{
+    using System.Linq;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <threadsafety static="true" instance="true"/>
+    public sealed class MinimizeMovesStrategy<TGame, TBoard, TMove, TPlayer> : IStrategy<TGame, TBoard, TMove, TPlayer> where TGame : IGame<TGame, TBoard, TMove, TPlayer>
+    {
+        private MinimizeMovesStrategy()
+        {
+        }
+
+        internal static readonly MinimizeMovesStrategy<TGame, TBoard, TMove, TPlayer> Default = new MinimizeMovesStrategy<TGame, TBoard, TMove, TPlayer>();
+
+        public TMove SelectMove(TGame game)
+        {
+            return game.Moves.Minimum(move => game.CommitMove(move).Moves.Count());
+        }
+    }
+}
