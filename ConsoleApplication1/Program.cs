6f2f519b3021c742b897a7d8d0daaca92dbcc407
Christof Sprenger <chrispre@microsoft.com>
2022-07-31 17:52:42 -0700

    random strategy

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -12,6 +12,7 @@
         {
             var displayer = new TicTacToeConsoleDisplayer<string>(_ => _);
             var consoleStrategy = new UserInterfaceStrategy<TicTacToe<string>, TicTacToeBoard, TicTacToeMove, string>(displayer);
+            var randomStrategy = new RandomStrategy<TicTacToe<string>, TicTacToeBoard, TicTacToeMove, string>();
             var chrispre = "chrispre";
             var gdebruin = "gdebruin";
             var game = new TicTacToe<string>(chrispre, gdebruin);
@@ -19,7 +20,7 @@
                 new Dictionary<string, IStrategy<TicTacToe<string>, TicTacToeBoard, TicTacToeMove, string>>
                 {
                     { chrispre, consoleStrategy },
-                    { gdebruin, consoleStrategy },
+                    { gdebruin, randomStrategy },
                 }, 
                 displayer);
             var result = driver.Run(game);
--- Fx.Games/UserInterfaceStrategy.cs
+++ Fx.Games/UserInterfaceStrategy.cs
@@ -1,6 +1,7 @@
 ﻿namespace Fx.Games
 {
     using System;
+    using System.Collections.Generic;
     using System.Linq;
 
     /// <summary>
@@ -46,6 +47,35 @@
             }*/
 
             return this.displayer.ReadMoveSelection(game);
+        }
+    }
+
+
+    public sealed class RandomStrategy<TGame, TBoard, TMove, TPlayer> : IStrategy<TGame, TBoard, TMove, TPlayer> where TGame : IGame<TGame, TBoard, TMove, TPlayer>
+    {
+        private readonly Random rng;
+
+        public RandomStrategy()
+        {
+            this.rng = new Random();
+        }
+
+        public TMove SelectMove(TGame game)
+        {
+
+            var moves = game.Moves.ToList();
+            var move = rng.Choose(moves);
+            return move;
+        }
+    }
+
+
+    public static class RandomExtensions
+    {
+        public static T Choose<T>(this Random rng, IReadOnlyList<T> items)
+        {
+            var ix = rng.Next(0, items.Count);
+            return items[ix];
         }
     }
 }
