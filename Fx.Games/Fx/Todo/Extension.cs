1966c57aff1fb353ada59df65a962ed4094095b8
Garrett DeBruin <corranrogue9@gmail.com>
2023-05-05 02:33:55 -0700

    asdf

--- Fx.Games/Fx/Todo/Extension.cs
+++ Fx.Games/Fx/Todo/Extension.cs
@@ -6,6 +6,7 @@
 
     using Fx.Game;
     using Fx.Tree;
+    using Fx.TreeFactory;
 
     /// <summary>
     /// 
@@ -27,6 +28,36 @@
                 action();
                 return new Void();
             };
+        }
+
+        public static ITree<T> CreateFromBranches<T>(IEnumerable<IEnumerable<T>> branches, ITreeFactory treeFactory)
+        {
+            if (!branches.Any())
+            {
+                throw new ArgumentException("TODO");
+            }
+
+            var branch = branches.First();
+            if (!branch.Any())
+            {
+                throw new ArgumentException("TODO");
+            }
+
+            var value = branch.First();
+
+
+            var subbranches = branches.Select(b => b.Skip(1)).Where(b => b.Any()).GroupBy(b => b.First());
+
+            return treeFactory.CreateInner(value, subbranches.Select(b => CreateFromBranches(b, treeFactory)));
+        }
+
+        internal static T Choose<T>(this IEnumerable<T> source, Func<T, bool> preference, Func<T, bool> fallback)
+        {
+            return source
+                .Aggregate(
+                    (0, default(T)),
+                    (aggregation, current) => aggregation.Item1 == 2 ? aggregation : preference(current) ? (2, current) : aggregation.Item1 == 1 ? aggregation : fallback(current) ? (1, current) : (0, current))
+                .Item2;
         }
 
         public static T[][] ToArray<T>(this T[,] source)
