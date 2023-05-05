66188c4b0fac6cf460a3c19c37e476d6c4bfac55
Garrett DeBruin <corranrogue9@gmail.com>
2023-05-05 02:59:19 -0700

    asdf

--- Fx.Games/Fx/Todo/Extension.cs
+++ Fx.Games/Fx/Todo/Extension.cs
@@ -68,20 +68,5 @@
                 },
                 Node.TreeFactory);
         }
-
-        public static T[][] ToArray<T>(this T[,] source)
-        {
-            var result = new T[source.GetLength(0)][];
-            for (int i = 0; i < result.Length; ++i)
-            {
-                result[i] = new T[source.GetLength(1)];
-                for (int j = 0; j < result[i].Length; ++j)
-                {
-                    result[i][j] = source[i, j];
-                }
-            }
-
-            return result;
-        }
     }
 }
