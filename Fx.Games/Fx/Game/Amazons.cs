9bc615a48dc2b959672f1281e40a41da3a9b2c88
Garrett DeBruin <corranrogue9@gmail.com>
2023-05-05 01:24:47 -0700

    asdf

--- Fx.Games/Fx/Game/Amazons.cs
+++ Fx.Games/Fx/Game/Amazons.cs
@@ -3,6 +3,8 @@
     using System;
     using System.Collections.Generic;
 
+    using Fx.Todo;
+
     /// <summary>
     /// TODO doesn't work probably
     /// </summary>
--- Fx.Games/Fx/Todo/ChessBoard.cs
+++ Fx.Games/Fx/Todo/ChessBoard.cs
@@ -1,4 +1,4 @@
-﻿namespace Fx.Game
+﻿namespace Fx.Todo
 {
     /// <summary>
     /// 
