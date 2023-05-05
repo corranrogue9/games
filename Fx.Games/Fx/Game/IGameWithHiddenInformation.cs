c2c7f357c1da8ba4de9e578f17780c58a841dc89
Garrett DeBruin <corranrogue9@gmail.com>
2023-05-05 03:51:00 -0700

    asdf

--- Fx.Games/Fx/Game/IGameWithHiddenInformation.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Fx.Game
-{
-    public interface IGameWithHiddenInformation<TMove, TPlayer, TGame> : Fx.Game.IGame<TMove, TPlayer, TGame> where TGame : IGameWithHiddenInformation<TMove, TPlayer, TGame>
-    {
-        IEnumerable<TGame> ExploreMove(TMove move);
-    }
-}
