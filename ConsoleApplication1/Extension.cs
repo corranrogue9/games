340a25251da5c2edcfe3dbc85dee8d3100a51f8d
Garrett DeBruin <corranrogue9@gmail.com>
2023-05-05 02:23:16 -0700

    asdf

---
+++ ConsoleApplication1/Extension.cs
@@ -0,0 +1,108 @@
+﻿namespace Fx.Todo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Fx.Game;
+    using Fx.Tree;
+    using Fx.TreeFactory;
+
+    internal static class Extension
+    {
+        public static bool TryFirst<T>(this IEnumerable<T> source, out T value)
+        {
+            using (var enumerator = source.GetEnumerator())
+            {
+                if (enumerator.MoveNext())
+                {
+                    value = enumerator.Current;
+                    return true;
+                }
+
+                value = default;
+                return false;
+            }
+        }
+
+        public static void Enumerate<T>(this IEnumerable<T> source)
+        {
+            foreach (var element in source)
+            {
+            }
+        }
+
+        public static T[][] ToArray<T>(this T[,] source)
+        {
+            var result = new T[source.GetLength(0)][];
+            for (int i = 0; i < result.Length; ++i)
+            {
+                result[i] = new T[source.GetLength(1)];
+                for (int j = 0; j < result[i].Length; ++j)
+                {
+                    result[i][j] = source[i, j];
+                }
+            }
+
+            return result;
+        }
+
+        public static Func<Void> ToFunc(Action action)
+        {
+            return () =>
+            {
+                action();
+                return new Void();
+            };
+        }
+
+        internal static ITree<IGame<TGame, TBoard, TMove, TPlayer>> ToTree<TGame, TBoard, TMove, TPlayer>(this IGame<TGame, TBoard, TMove, TPlayer> game) where TGame : IGame<TGame, TBoard, TMove, TPlayer>
+        {
+            if (game.Outcome == null)
+            {
+                return Node.CreateTree(game, game.Moves.Select(move => game.CommitMove(move).ToTree()));
+            }
+            else
+            {
+                return Node.CreateTree(game);
+            }
+        }
+
+        internal static T Choose<T>(this IEnumerable<T> source, Func<T, bool> preference, Func<T, bool> fallback)
+        {
+            return source
+                .Aggregate(
+                    (0, default(T)),
+                    (aggregation, current) => aggregation.Item1 == 2 ? aggregation : preference(current) ? (2, current) : aggregation.Item1 == 1 ? aggregation : fallback(current) ? (1, current) : (0, current))
+                .Item2;
+        }
+
+
+        public static IEnumerable<IEnumerable<T>> EnumerateBranches<T>(this ITree<T> tree)
+        {
+            return tree.Fold(
+                (value) => new[] { new[] { value }.AsEnumerable() }.AsEnumerable(),
+                (value, aggregation) => aggregation.SelectMany(child => child.Select(branch => branch.Prepend(value))));
+        }
+
+        public static ITree<T> CreateFromBranches<T>(IEnumerable<IEnumerable<T>> branches, ITreeFactory treeFactory)
+        {
+            if (!branches.Any())
+            {
+                throw new ArgumentException("TODO");
+            }
+
+            var branch = branches.First();
+            if (!branch.Any())
+            {
+                throw new ArgumentException("TODO");
+            }
+
+            var value = branch.First();
+
+
+            var subbranches = branches.Select(b => b.Skip(1)).Where(b => b.Any()).GroupBy(b => b.First());
+
+            return treeFactory.CreateInner(value, subbranches.Select(b => CreateFromBranches(b, treeFactory)));
+        }
+    }
+}
