add0f9dfec1f65894bb5f468d640d15bc4e02ed6
Garrett DeBruin <corranrogue9@gmail.com>
2023-05-05 03:48:44 -0700

    asdf

--- Fx.Games/Fx/Strategy/IStrategy{TMove, TPlayer, TGame}.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Fx.Strategy
-{
-    using Fx.Game;
-
-    public interface IStrategy<TMove, TPlayer, TGame> where TGame : IGame<TMove, TPlayer, TGame>
-    {
-        TMove SelectMove(TGame game);
-    }
-}
