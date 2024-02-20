f1c3e748f1ed66a31370b445e52955d77139f09f
Garrett DeBruin <corranrogue9@gmail.com>
2024-02-20 10:24:38 -0800

    asdf

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -329,9 +329,9 @@
                 {
                     ////{ exes, UserInterfaceStrategy.Create(displayer) },
                     ////{ exes, game.RandomStrategy() },
-                    ////{ exes, new MonteCarloStrategy2<Longhorn<string>, LonghornBoard<string>, LonghornMove, string>(exes, 100000, StringComparer.OrdinalIgnoreCase, random) },
+                    { exes, new MonteCarloStrategy2<Longhorn<string>, LonghornBoard<string>, LonghornMove, string>(exes, 100000, StringComparer.OrdinalIgnoreCase, random) },
                     { ohs, game.RandomStrategy() },
-                    { exes, new DecisionTreeStrategy<Longhorn<string>, LonghornBoard<string>, LonghornMove, string>(exes, StringComparer.OrdinalIgnoreCase) },
+                    ////{ ohs, new DecisionTreeStrategy<Longhorn<string>, LonghornBoard<string>, LonghornMove, string>(ohs, StringComparer.OrdinalIgnoreCase) },
                 },
                 displayer);
             Console.WriteLine(seed);
