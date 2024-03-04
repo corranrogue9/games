de6cac9554b19c8f9965255358bd9bfac30698e3
Garrett DeBruin <corranrogue9@gmail.com>
2024-03-03 19:26:13 -0800

    adding an undo game

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -8,6 +8,7 @@
     using System.Numerics;
     using System.Runtime.CompilerServices;
     using System.Runtime.Versioning;
+    using System.Text;
     using Fx.Displayer;
     using Fx.Driver;
     using Fx.Game;
@@ -315,24 +316,343 @@
             var result = driver.Run(game);
         }
 
+        public sealed class LonghonrUndoDisplayer<TPlayer> : IDisplayer<UndoableGame<Longhorn<TPlayer>, LonghornBoard<TPlayer>, LonghornMove, TPlayer>, LonghornBoard<TPlayer>, UndoableMove<LonghornMove>, TPlayer>
+        {
+            //// TODO we already knew displaying was trash, but an undo displayer can't even be general purpose or compose the existing longhorn displayer; it's *really* bad
+
+            private readonly Func<TPlayer, string> transcriber;
+
+            public LonghonrUndoDisplayer(Func<TPlayer, string> transcriber)
+            {
+                this.transcriber = transcriber;
+            }
+
+            public void DisplayBoard(UndoableGame<Longhorn<TPlayer>, LonghornBoard<TPlayer>, LonghornMove, TPlayer> game)
+            {
+                Console.WriteLine();
+                Console.WriteLine();
+
+                Console.WriteLine($"{this.transcriber(game.Board.Player1Status.Player)} status: {game.Board.Player1Status.Black} black cows, {game.Board.Player1Status.Green} green cows, {game.Board.Player1Status.Orange} orange cows, {game.Board.Player1Status.White} white cows; {string.Join(", ", game.Board.Player1Status.GoldNuggets)} gold nuggets");
+                Console.WriteLine($"{this.transcriber(game.Board.Player2Status.Player)} status: {game.Board.Player2Status.Black} black cows, {game.Board.Player2Status.Green} green cows, {game.Board.Player2Status.Orange} orange cows, {game.Board.Player2Status.White} white cows; {string.Join(", ", game.Board.Player2Status.GoldNuggets)} gold nuggets");
+                Console.WriteLine();
+
+                var writer = new Writer();
+                for (int i = 0; i < 3; ++i)
+                {
+                    for (int j = 0; j < 3; ++j)
+                    {
+                        var tile = game.Board.Tiles[i, j];
+                        var playerLocation = game.Board.PlayerLocation;
+                        var currentPlayerLocation = playerLocation != null && playerLocation.Row == i && playerLocation.Column == j;
+
+                        if (tile.ActionToken is ActionToken.Ambush)
+                        {
+                            writer.Append(i * 8 + 0, $"action token: ambush");
+                        }
+                        else if (tile.ActionToken is ActionToken.BrandingIron)
+                        {
+                            writer.Append(i * 8 + 0, $"action token: branding iron");
+                        }
+                        else if (tile.ActionToken is ActionToken.Epidemic)
+                        {
+                            writer.Append(i * 8 + 0, $"action token: epidemice");
+                        }
+                        else if (tile.ActionToken is ActionToken.Gold gold)
+                        {
+                            writer.Append(i * 8 + 0, $"action token: gold ({gold.Amount})");
+                        }
+                        else if (tile.ActionToken is ActionToken.Rattlesnake)
+                        {
+                            writer.Append(i * 8 + 0, $"action token: rattlesnake");
+                        }
+                        else if (tile.ActionToken is ActionToken.Sheriff)
+                        {
+                            writer.Append(i * 8 + 0, $"action token: sheriff");
+                        }
+                        else if (tile.ActionToken is ActionToken.SnakeOil)
+                        {
+                            writer.Append(i * 8 + 0, $"action token: snake oil");
+                        }
+                        else
+                        {
+                            writer.Append(i * 8 + 0, $"");
+                        }
+
+                        writer.Append(i * 8 + 1, $"green cows: {tile.GreenCows}");
+                        writer.Append(i * 8 + 2, $"black cows: {tile.BlackCows}");
+                        writer.Append(i * 8 + 3, $"white cows: {tile.WhiteCows}");
+                        writer.Append(i * 8 + 4, $"orange cows: {tile.OrangeCows}");
+
+                        if (currentPlayerLocation)
+                        {
+                            writer.Append(i * 8 + 5, $"CURRENT PLAYER LOCATION!");
+                        }
+                        else
+                        {
+                            writer.Append(i * 8 + 5, string.Empty);
+                        }
+                    }
+                }
+
+                Console.WriteLine(writer.Transcribe());
+            }
+
+            private sealed class Writer
+            {
+                private readonly int columnPadding;
+
+                private readonly Dictionary<int, List<string>> lines;
+
+                private readonly Dictionary<int, int> columnSizes;
+
+                public Writer()
+                    : this(2)
+                {
+                }
+
+                public Writer(int columnPadding)
+                {
+                    this.columnPadding = columnPadding;
+
+                    this.lines = new Dictionary<int, List<string>>();
+                    this.columnSizes = new Dictionary<int, int>();
+                }
+
+                public void Append(int lineNumber, string value)
+                {
+                    if (!this.lines.TryGetValue(lineNumber, out var columns))
+                    {
+                        columns = new List<string>();
+                        this.lines[lineNumber] = columns;
+                    }
+
+                    if (!this.columnSizes.TryGetValue(columns.Count, out var columnSize))
+                    {
+                        columnSize = 0;
+                    }
+
+                    this.columnSizes[columns.Count] = Math.Max(value.Length, columnSize);
+                    columns.Add(value);
+                }
+
+                public string Transcribe()
+                {
+                    var builders = new List<StringBuilder>();
+                    foreach (var (lineNumber, columns) in this.lines)
+                    {
+                        while (lineNumber >= builders.Count)
+                        {
+                            builders.Add(new StringBuilder());
+                        }
+
+                        var builder = builders[lineNumber];
+                        for (int i = 0; i < columns.Count; ++i)
+                        {
+                            var column = columns[i];
+                            builder.Append(column);
+                            var columnSize = this.columnSizes[i];
+                            builder.Append(new string(' ', columnSize - column.Length + this.columnPadding));
+                        }
+                    }
+
+                    var transcriber = new StringBuilder();
+                    foreach (var builder in builders)
+                    {
+                        transcriber.AppendLine(builder.ToString());
+                    }
+
+                    return transcriber.ToString();
+                }
+            }
+
+            public void DisplayMoves(UndoableGame<Longhorn<TPlayer>, LonghornBoard<TPlayer>, LonghornMove, TPlayer> game)
+            {
+                Console.WriteLine("Which move would you like to select?");
+                int i = 0;
+                foreach (var move in game.Moves)
+                {
+                    Console.WriteLine($"{i}: {TranscribeMove(game, move)}");
+
+                    ++i;
+                }
+            }
+
+            public string TranscribeMove(UndoableGame<Longhorn<TPlayer>, LonghornBoard<TPlayer>, LonghornMove, TPlayer> game, UndoableMove<LonghornMove> move)
+            {
+                if (move is UndoableMove<LonghornMove>.Native longhornMove)
+                {
+                    if (longhornMove.Value is LonghornMove.LocationChoice locationChoice)
+                    {
+                        return $"place the other player at ({locationChoice.Location.Row}, {locationChoice.Location.Column})";
+                    }
+                    else if (longhornMove.Value is LonghornMove.LocationMove locationMove)
+                    {
+                        var newLocation = locationMove.NewLocation;
+                        string resultLocation;
+                        if (newLocation == null)
+                        {
+                            resultLocation = "end the game";
+                        }
+                        else
+                        {
+                            resultLocation = $"move to ({newLocation.Row}, {newLocation.Column})";
+                        }
+
+                        //// TODO actionmove is null for several types of action tokens; this is because there's no decision to be made for those tokens; however, 
+                        //// TODO displaying the move, we will likely want to show the user the action move that they will be committing; in this case, the displayer
+                        //// TODO will be responsible for knowing what the action move is that is being committed; this violates the idea that the game engine is the
+                        //// TODO only thing responsible for knowing how to play the game; on the other hand, if the game engine requires the action move to be
+                        //// TODO supplied, it's a pretty bad experience to commit a move that is legal except that the caller needed to know that the action move is
+                        //// TODO the remaining legal move; is there a way to improve the framework to account for such things? maybe the modeling of the move for this
+                        //// TODO game as having 3 components is the broken part?
+                        var action = string.Empty;
+                        if (locationMove.ActionMove is ActionMove.Ambush.StealGold ambushGold)
+                        {
+                            action = ", steal a random gold nugget from your opponent,";
+                        }
+                        else if (locationMove.ActionMove is ActionMove.Ambush.StealCows ambushCows)
+                        {
+                            action = $", steal the {ambushCows.Color} cows from your opponent,";
+                        }
+                        else if (locationMove.ActionMove is ActionMove.BrandingIron brandingIron)
+                        {
+                            action = $", brand the {locationMove.TakeColor} cows at ({brandingIron.Location.Row}, {brandingIron.Location.Column}),";
+                        }
+                        else if (locationMove.ActionMove is ActionMove.Epidemic epidemic)
+                        {
+                            action = $", cause an epidemic among the {epidemic.Color} cows,";
+                        }
+                        else if (locationMove.ActionMove is ActionMove.Gold)
+                        {
+                            action = $", take the gold token,";
+                        }
+                        else if (locationMove.ActionMove is ActionMove.Rattlesnake rattlesnake)
+                        {
+                            var locations = string.Join(", ", new[]
+                                {
+                                    (TakeColor.Black, rattlesnake.BlackLocation),
+                                    (TakeColor.Green, rattlesnake.GreenLocation),
+                                    (TakeColor.Orange, rattlesnake.OrangeLocation),
+                                    (TakeColor.White, rattlesnake.WhiteLocation),
+                                }
+                                .Where(location => location.Item2 != null)
+                                .Select(location => $"{location.Item1} cows to ({location.Item2.Row}, {location.Item2.Column})"));
+
+                            action = $", guide your cows away from the rattlesnake ({locations}),";
+                        }
+                        else if (locationMove.ActionMove is ActionMove.Sheriff)
+                        {
+                            action = $", get caught by the sheriff,";
+                        }
+                        else if (locationMove.ActionMove is ActionMove.SnakeOil)
+                        {
+                            action = $", use the snake oil to take an extra turn,";
+                        }
+
+                        return $"take {CowCount(game.Game, locationMove, game.Board.PlayerLocation)} {locationMove.TakeColor} cows{action} and {resultLocation}";
+                    }
+
+                    throw new InvalidOperationException($"invalid move type: {longhornMove.GetType()}");
+                }
+                else
+                {
+                    return "undo";
+                }
+            }
+
+            private static int CowCount(Longhorn<TPlayer> game, LonghornMove.LocationMove locationMove, LonghornLocation location)
+            {
+                var tile = game.Board.Tiles[location.Row, location.Column];
+                if (locationMove.TakeColor == TakeColor.Black)
+                {
+                    return tile.BlackCows;
+                }
+
+                if (locationMove.TakeColor == TakeColor.Green)
+                {
+                    return tile.GreenCows;
+                }
+
+                if (locationMove.TakeColor == TakeColor.Orange)
+                {
+                    return tile.OrangeCows;
+                }
+
+                if (locationMove.TakeColor == TakeColor.White)
+                {
+                    return tile.WhiteCows;
+                }
+
+                throw new InvalidOperationException($"invalid color specified for move: {(int)locationMove.TakeColor}");
+            }
+
+            public void DisplayOutcome(UndoableGame<Longhorn<TPlayer>, LonghornBoard<TPlayer>, LonghornMove, TPlayer> game)
+            {
+                Console.WriteLine($"{string.Join(",", game.Outcome.Winners)} won the game!");
+            }
+
+            public UndoableMove<LonghornMove> ReadMoveSelection(UndoableGame<Longhorn<TPlayer>, LonghornBoard<TPlayer>, LonghornMove, TPlayer> game)
+            {
+                var moves = game.Moves.ToList();
+                while (true)
+                {
+                    var input = Console.ReadLine();
+                    if (!int.TryParse(input, out var selectedMove) || selectedMove >= moves.Count)
+                    {
+                        Console.WriteLine($"The input '{input}' was not the index of a legal move");
+                        continue;
+                    }
+
+                    Console.WriteLine(selectedMove);
+                    return moves[selectedMove];
+                }
+            }
+        }
+
+        public static class NonundoableStrategy
+        {
+            public static NonundoableStrategy<TGame, TBoard, TMove, TPlayer> Create<TGame, TBoard, TMove, TPlayer>(IStrategy<TGame, TBoard, TMove, TPlayer> strategy)
+                where TGame : IGame<TGame, TBoard, TMove, TPlayer>
+            {
+                return new NonundoableStrategy<TGame, TBoard, TMove, TPlayer>(strategy);
+            }
+        }
+
+        public sealed class NonundoableStrategy<TGame, TBoard, TMove, TPlayer> : IStrategy<UndoableGame<TGame, TBoard, TMove, TPlayer>, TBoard, UndoableMove<TMove>, TPlayer>
+            where TGame : IGame<TGame, TBoard, TMove, TPlayer>
+        {
+            private readonly IStrategy<TGame, TBoard, TMove, TPlayer> strategy;
+
+            public NonundoableStrategy(IStrategy<TGame, TBoard, TMove, TPlayer> strategy)
+            {
+                this.strategy = strategy;
+            }
+
+            public UndoableMove<TMove> SelectMove(UndoableGame<TGame, TBoard, TMove, TPlayer> game)
+            {
+                return new UndoableMove<TMove>.Native(this.strategy.SelectMove(game.Game));
+            }
+        }
+
         private static void LonghornHumanVsRandom()
         {
-            var displayer = new LonghornConsoleDisplay<string>(_ => _);
+            var displayer = new LonghonrUndoDisplayer<string>(_ => _);
             ////var seed = 581316343;
             var seed = Environment.TickCount;
             var random = new Random(seed);
             var exes = "player1";
             var ohs = "player2";
-            var game = new Longhorn<string>(exes, ohs, random);
+            var longhorn = new Longhorn<string>(exes, ohs, random);
+            var game = new UndoableGame<Longhorn<string>, LonghornBoard<string>, LonghornMove, string>(longhorn);
             var driver = Driver.Create(
-                new Dictionary<string, IStrategy<Longhorn<string>, LonghornBoard<string>, LonghornMove, string>>
+                new Dictionary<string, IStrategy<UndoableGame<Longhorn<string>, LonghornBoard<string>, LonghornMove, string>, LonghornBoard<string>, UndoableMove<LonghornMove>, string>>
                 {
                     { exes, UserInterfaceStrategy.Create(displayer) },
                     ////{ exes, game.RandomStrategy() },
                     ////{ exes, new MonteCarloStrategy2<Longhorn<string>, LonghornBoard<string>, LonghornMove, string>(exes, 100000, StringComparer.OrdinalIgnoreCase, random) },
                     ////{ ohs, game.RandomStrategy() },
                     ////{ ohs, UserInterfaceStrategy.Create(displayer) },
-                    { ohs, new MonteCarloStrategy2<Longhorn<string>, LonghornBoard<string>, LonghornMove, string>(ohs, 100000, StringComparer.OrdinalIgnoreCase, random) },
+                    { ohs, NonundoableStrategy.Create(new MonteCarloStrategy2<Longhorn<string>, LonghornBoard<string>, LonghornMove, string>(ohs, 100000, StringComparer.OrdinalIgnoreCase, random)) },
                     ////{ ohs, new DecisionTreeStrategy<Longhorn<string>, LonghornBoard<string>, LonghornMove, string>(ohs, StringComparer.OrdinalIgnoreCase) },
                 },
                 displayer);
---
+++ Fx.Games/Fx/Game/UndoableGame/UndoableGame.cs
@@ -0,0 +1,125 @@
+﻿using System.Linq;
+
+namespace Fx.Game
+{
+    public sealed class UndoableGame<TGame, TBoard, TMove, TPlayer> : IGame<UndoableGame<TGame, TBoard, TMove, TPlayer>, TBoard, UndoableMove<TMove>, TPlayer>
+        where TGame : IGame<TGame, TBoard, TMove, TPlayer>
+    {
+        private readonly TGame game;
+
+        private readonly UndoableGame<TGame, TBoard, TMove, TPlayer>? previous;
+
+        public UndoableGame(TGame game)
+        {
+            if (game == null)
+            {
+                throw new ArgumentNullException(nameof(game));
+            }
+
+            this.game = game;
+            this.previous = null; //// TODO it should really undo until the player's last turn
+        }
+
+        private UndoableGame(TGame game, UndoableGame<TGame, TBoard, TMove, TPlayer> previous)
+        {
+            this.game = game;
+            this.previous = previous;
+        }
+
+        public TGame Game
+        {
+            get
+            {
+                return this.game;
+            }
+        }
+
+        public TPlayer CurrentPlayer
+        {
+            get
+            {
+                return this.game.CurrentPlayer;
+            }
+        }
+
+        public IEnumerable<UndoableMove<TMove>> Moves
+        {
+            get
+            {
+                /*return this
+                    .game
+                    .Moves
+                    .Select(move => new UndoableMove<TMove>.Native(move))
+                    .Prepend(UndoableMove<TMove>.Undo.Instance);*/ //// TODO never ran into this before...
+                if (this.previous != null)
+                {
+                    yield return UndoableMove<TMove>.Undo.Instance;
+                }
+
+                foreach (var element in this.game.Moves.Select(move => new UndoableMove<TMove>.Native(move)))
+                {
+                    yield return element;
+                }
+            }
+        }
+
+        public TBoard Board
+        {
+            get
+            {
+                return this.game.Board;
+            }
+        }
+
+        public Outcome<TPlayer> Outcome
+        {
+            get
+            {
+                return this.game.Outcome;
+            }
+        }
+
+        public UndoableGame<TGame, TBoard, TMove, TPlayer> CommitMove(UndoableMove<TMove> move)
+        {
+            if (move is UndoableMove<TMove>.Native native)
+            {
+                return new UndoableGame<TGame, TBoard, TMove, TPlayer>(this.game.CommitMove(native.Value), this);
+            }
+            else
+            {
+                if (this.previous == null)
+                {
+                    throw new IllegalMoveExeption("Attempted to undo the first move of the game");
+                }
+
+                return this.previous;
+            }
+        }
+    }
+
+    public abstract class UndoableMove<TMove>
+    {
+        private UndoableMove()
+        {
+        }
+
+        public sealed class Undo : UndoableMove<TMove>
+        {
+            private Undo()
+            {
+            }
+
+            public static Undo Instance { get; } = new Undo();
+        }
+
+        public sealed class Native : UndoableMove<TMove>
+        {
+            public Native(TMove value)
+            {
+                this.Value = value;
+            }
+
+            public TMove Value { get; }
+        }
+    }
+}
